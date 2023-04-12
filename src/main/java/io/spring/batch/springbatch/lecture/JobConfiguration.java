d166fe91267390e49334f1149abd3bb65e5777f5
injinjeong <snowwwwww@naver.com>
2023-04-12 09:57:30 +0900

    JobConfiguration yml

--- src/main/java/io/spring/batch/springbatch/lecture/JobConfiguration.java
+++ src/main/java/io/spring/batch/springbatch/lecture/JobConfiguration.java
@@ -2,32 +2,25 @@ package io.spring.batch.springbatch.lecture;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobRepositoryConfiguration {
+public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    private final JobExecutionListener jobExecutionListener;
-
     @Bean
     public Job BatchJob() {
-        return jobBuilderFactory.get("job")
+        return jobBuilderFactory.get("batchJob1")
                 .start(step1())
                 .next(step2())
-                .listener(jobExecutionListener)
                 .build();
     }
 
