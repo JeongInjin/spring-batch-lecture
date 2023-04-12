939974cc19873b32ad716d715ef347971c7ca2fb
injinjeong <snowwwwww@naver.com>
2023-04-12 14:53:45 +0900

    incrementer

--- src/main/java/io/spring/batch/springbatch/lecture/IncrementerConfiguration.java
+++ src/main/java/io/spring/batch/springbatch/lecture/IncrementerConfiguration.java
@@ -5,13 +5,14 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+// --job.name=batchJob name=user1 date=20230101
 @Configuration
 @RequiredArgsConstructor
-public class PreventRestartConfiguration {
+public class IncrementerConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -21,14 +22,18 @@ public class PreventRestartConfiguration {
         return jobBuilderFactory.get("batchJob1")
                 .start(step1())
                 .next(step2())
-                .preventRestart()
+//                .incrementer(new CustomJobParametersIncrementer())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step1 has executed");
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 
@@ -36,7 +41,7 @@ public class PreventRestartConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
-//                    throw new RuntimeException("step2 has failed");
+                    System.out.println("step2 has executed");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
