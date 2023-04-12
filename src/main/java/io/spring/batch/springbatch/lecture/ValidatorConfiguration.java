f618e8640a43a128052d4f8ca46fdd258d73f38c
injinjeong <snowwwwww@naver.com>
2023-04-12 13:59:41 +0900

    validator

--- src/main/java/io/spring/batch/springbatch/lecture/ValidatorConfiguration.java
+++ src/main/java/io/spring/batch/springbatch/lecture/ValidatorConfiguration.java
@@ -5,13 +5,14 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobConfiguration {
+public class ValidatorConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -21,6 +22,9 @@ public class JobConfiguration {
         return jobBuilderFactory.get("batchJob1")
                 .start(step1())
                 .next(step2())
+                .next(step3())
+//                .validator(new CustomJobParametersValidator())
+                .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
                 .build();
     }
 
@@ -34,6 +38,13 @@ public class JobConfiguration {
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
                 .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
     }
